60d23a6c1118082095935c167ce23c35f64acc8a
vagrant <vagrant@homestead>
2016-09-29 01:42:52 +0000

    slider, changed elessar.js delete function on line 326 so it is shift + click to delete. Before it had to be right click, which was impossible because dom intercepted right click with contextmenu event

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const elixir = require('laravel-elixir');
+
+require('laravel-elixir-vue');
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+elixir(mix => {
+    mix.copy('resources\assets\bower\elessar\demos\index.html', 'resources\views\time.blade.php');
+});
